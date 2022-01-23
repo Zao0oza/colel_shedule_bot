875abda226ecac8e98dc74a6411460cc77a4cc60
Zao0oza <sefeigin@mail.ru>
2022-01-23 18:16:53 +0300

    google spread version

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import yaml
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher.filters import Text
 from bottoken import token
+import gspread
+
+gc = gspread.service_account(filename='regal-eon-313211-848c0cdd83c0.json')
 
 # Объект бота
 bot = Bot(token=token)
@@ -12,14 +15,22 @@ dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
 
 
+def get_pupils():
+    wks = gc.open_by_url(
+        'https://docs.google.com/spreadsheets/d/1ljIQbUyL_RMii47joDgWRfXr4q15e4mSw1oA1ZxI8D0/edit#gid=0')
+    worksheet = wks.get_worksheet(0)
+    list_of_dicts = worksheet.get_all_values()
+    pupildict = {v: [k, j] for v, k, j in list_of_dicts[1:]}
+    return pupildict
+
+
 # функция отвечающая за распиание
-def colel_schedule(pupildict:dict):
+def colel_schedule():
     i = 0
     """
-    Получаем словарь со списком людей отсортированный по количеству дежурств
+    Получаем словарь и сортируем по списку дежурств
     """
-    sortedpupils = dict(sorted(pupildict.items(), key=lambda x: x[1]['quantity']))
-    print(sortedpupils)
+    sortedpupils = dict(sorted(get_pupils().items(), key=lambda x: x[1][0]))
     pupilres = list(sortedpupils.keys())[0:5]
     """
     Проверям сколько хватает ли людей на 5 дней
@@ -35,9 +46,12 @@ def colel_schedule(pupildict:dict):
     Записываем их в файл
     """
     for i in pupilres:
-        pupildict[i]['quantity'] += 1
-    with open('pupils.yaml', 'w') as file:
-        documents = yaml.dump(pupildict, file)
+        wks = gc.open_by_url(
+            'https://docs.google.com/spreadsheets/d/1ljIQbUyL_RMii47joDgWRfXr4q15e4mSw1oA1ZxI8D0/edit#gid=0')
+        worksheet = wks.get_worksheet(0)
+        cell = worksheet.find(i)
+        val=int(worksheet.cell(cell.row, cell.col+1).value)+1
+        worksheet.update(f'R{cell.row}C{cell.col + 1}', val)
     return pupilres
 
 
@@ -58,20 +72,15 @@ async def cmd_start(message: types.Message):
 @dp.message_handler(Text(equals="Расписание"))
 async def with_puree(message: types.Message):
     # загружаем актуальный список учеников
-    try:
-        with open('pupils.yaml') as f:
-            pupildict = yaml.safe_load(f)
-            await message.reply(colel_schedule(pupildict))
-    except:
-        await message.reply("Добавьте учеников")
+    await message.reply(colel_schedule())
+    #await message.reply("Добавьте учеников")
 
 
 @dp.message_handler(lambda message: message.text == "Список учеников")
 async def without_puree(message: types.Message):
     # загружаем актуальный список учеников
-    with open('pupils.yaml') as f:
-        pupildict = yaml.safe_load(f)
-    await message.answer(pupildict)
+    await message.answer(get_pupils())
+    #await message.answer("Добавьте учеников")
 
 
 if __name__ == "__main__":
