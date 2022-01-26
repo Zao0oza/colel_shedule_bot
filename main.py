5338fe26da61b7a49b2d843fd1b79f95cca7b5c9
Zao0oza <sefeigin@mail.ru>
2022-01-26 15:15:41 +0300

    keybord extend, add previous schedules

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher.filters import Text
 from bottoken import token, google_url
 import gspread
+import datetime
+import yaml
 
-
-gc = gspread.service_account(filename='regal-eon-313211-848c0cdd83c0.json') # ключ для google api
+gc = gspread.service_account(filename='regal-eon-313211-848c0cdd83c0.json')  # ключ для google api
 
 # Объект бота
 bot = Bot(token=token)
@@ -19,9 +20,10 @@ def colel_schedule():
     Получаем словарь и сортируем по списку дежурств
     Убираем отсутсвующих
     """
+
     try:
         wks = gc.open_by_url(
-            google_url)# url google документа
+            google_url)  # url google документа
         worksheet = wks.get_worksheet(0)
         list_of_dicts = worksheet.get_all_values()
         pupildict = {v: [k, j] for v, k, j in list_of_dicts[1:]}
@@ -49,11 +51,22 @@ def colel_schedule():
             cell = worksheet.find(i)
             val = int(worksheet.cell(cell.row, cell.col + 1).value) + 1
             worksheet.update(f'R{cell.row}C{cell.col + 1}', val)
-        return f"Воскресенье: {pupilres[0]}\nПонедельник:{pupilres[1]}\nВторник: {pupilres[2]}\nСреда: {pupilres[3]}\nЧетверг: {pupilres[4]}\n"
-    except :
+        result = pupilres[0:5]
+        result.insert(0, datetime.date.today())
+        with open('store_file.yaml', 'a') as f:
+            yaml.dump({datetime.datetime.now(): result}, f, default_flow_style=False, allow_unicode=True)
+
+        return result
+    except:
         return "проверьте список учеников!"
 
 
+def wrap_res(res_schedule: list):
+    # функция  для читабельного вывода результата
+    return f"Расписание на: {res_schedule[0]}\nВоскресенье: {res_schedule[1]}\nПонедельник:{res_schedule[2]}\nВторник: {res_schedule[3]}\nСреда: {res_schedule[4]}\nЧетверг: {res_schedule[5]}\n"
+
+
+@dp.message_handler(Text(equals="Назад"))
 @dp.message_handler(commands="start")
 async def cmd_start(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -63,18 +76,50 @@ async def cmd_start(message: types.Message):
 
 
 @dp.message_handler(Text(equals="Расписание"))
-async def with_puree(message: types.Message):
-    # загружаем актуальный список учеников
-    await message.answer(colel_schedule())
-    # await message.reply("Добавьте учеников")
+async def schedule_actual(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ["Новое расписание", "Прошлые расписания", "Назад"]
+    keyboard.add(*buttons)
+    try:
+        with open('store_file.yaml') as stream:
+            res_schedule = list(yaml.safe_load(stream).values())[-1]
+            try:
+                await message.answer(wrap_res(res_schedule), reply_markup=keyboard)
+            except:
+                await message.answer(wrap_res(colel_schedule()), reply_markup=keyboard)
+    except:
+        await message.answer(wrap_res(colel_schedule()), reply_markup=keyboard)
 
 
 @dp.message_handler(lambda message: message.text == "Список учеников")
-async def without_puree(message: types.Message):
-    # загружаем актуальный список учеников
+async def pupil_list(message: types.Message):
+    # даем ссылку на таблицу с учениками
     await message.answer(
         'https://docs.google.com/spreadsheets/d/1ljIQbUyL_RMii47joDgWRfXr4q15e4mSw1oA1ZxI8D0/edit#gid=0')
-    # await message.answer("Добавьте учеников")
+
+
+@dp.message_handler(lambda message: message.text == "Новое расписание")
+async def new_schedule(message: types.Message):
+    # даем ссылку на таблицу с учениками
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ["Новое расписание", "Прошлые расписания", "Назад"]
+    keyboard.add(*buttons)
+    await message.answer(wrap_res(colel_schedule()), reply_markup=keyboard)
+
+
+@dp.message_handler(lambda message: message.text == "Прошлые расписания")
+async def new_schedule(message: types.Message):
+    # даем ссылку на таблицу с учениками
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ["Новое расписание", "Прошлые расписания", "Назад"]
+    keyboard.add(*buttons)
+    try:
+        with open('store_file.yaml') as stream:
+            res_schedule = list(yaml.safe_load(stream).values())
+            for row in res_schedule:
+                await message.answer(' '.join(map(str, row)), reply_markup=keyboard)
+    except:
+        await message.answer("отсутствуют")
 
 
 if __name__ == "__main__":
